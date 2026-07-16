38a99c6aaa1ce1638d7bf451956a691000e9d07d
xyb85315266 <xueyb8518@163.com>
2026-07-16 18:18:08 +0800

    chore: 添加本地启动脚本 start-bd.bat（独立窗口拉起 backend-ts 与 desktop）

---
+++ start-bd.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" /min cmd /c "cd /d %~dp0apps\backend-ts && npm run dev > %~dp0logs\backend-ts\ts-backend.log 2>&1"
+start "" /min cmd /c "cd /d %~dp0apps\desktop && npm run dev > %~dp0logs\desktop\electron.log 2>&1"
